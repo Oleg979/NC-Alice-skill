ef30e744dcd6c6f03253e891a65861c79a5760eb
Oleg <olegsolovev506@gmail.com>
2019-10-30 11:31:00 +0400

    asdasd

--- controllers/AliceController.js
+++ controllers/AliceController.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get("/ping", async (req, res) => {
+router.post("/ping", async (req, res) => {
   const { request, session, version } = req.body;
   res.status(200).send({
     version,
