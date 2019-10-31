86c83311403f82edf5a70b2eaa0033b8168e27cd
Oleg <olegsolovev506@gmail.com>
2019-10-31 15:28:05 +0400

    fix

--- controllers/aliceController.js
+++ controllers/aliceController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const AliceService = require("../services/AliceService");
+const { START_MESSAGE, ERROR_MESSAGE } = require("../config/constants");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
@@ -22,6 +23,16 @@ router.post("/ping", async (req, res) => {
 router.post("/", async (req, res) => {
   const { request, session, version } = req.body;
   const text = request.original_utterance;
+  if (text == "")
+    return res.status(200).send({
+      version,
+      session,
+      response: {
+        text: START_MESSAGE,
+        tts: START_MESSAGE,
+        end_session: false
+      }
+    });
   try {
     const serverResponse = await AliceService.forwardToServer(text);
     res.status(200).send({
@@ -38,7 +49,8 @@ router.post("/", async (req, res) => {
       version,
       session,
       response: {
-        text: "Error",
+        text: ERROR_MESSAGE,
+        tts: ERROR_MESSAGE,
         end_session: false
       }
     });
