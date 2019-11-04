701eb8455ddbcc1466934e4846b6121eef5f9ff2
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2019-11-04 22:33:08 +0400

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,13 @@
 module.exports = {
-  START_MESSAGE: "Welcome to Netcracker. How may I help you?",
+  START_MESSAGE: `Я помогу тебе управлять умным домом и нашим веб-сайтом. Просто озвучь желаемое и увидишь результат. Примеры команд:
+1. Сколько у меня фильмов?
+2. Скрыть панель билетов
+3. Следующая страница.
+4. Отдать 1 билет Сэму.
+5. Я хочу посмотреть футбол.
+6. Какая температура в комнате Алисы?
+7. Измени локацию на Лондон.
+`,
   EXIT_MESSAGE: "Good bye.",
   ERROR_MESSAGE:
     "Sorry, but server is not available. Please, consult a specialist",
